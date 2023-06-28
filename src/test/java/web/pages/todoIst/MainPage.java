9cc3c818c36ba2cdfd46a09d082dd13f11723181
micky <mickymolina@gmail.com>
2023-06-28 17:40:50 -0400

    FirstCommit

--- src/test/java/web/pages/todoIst/MainPage.java
+++
@@ -1,8 +0,0 @@
-package web.pages.todoIst;
-
-import web.controls.Button;
-import org.openqa.selenium.By;
-
-public class MainPage {
-    public Button iniciarSesionButton = new Button(By.xpath("//a[contains(text(),'Iniciar')]"));
-}
--- src/test/java/web/pages/todoLy/MainPage.java
+++ src/test/java/web/pages/todoLy/MainPage.java
@@ -2,8 +2,11 @@ package web.pages.todoLy;
 
 import web.controls.Button;
 import org.openqa.selenium.By;
+import web.controls.Label;
 
 public class MainPage {
     public Button loginButton = new Button(By.xpath("//img[@src='/Images/design/pagelogin.png']"));
+    public Button signupBtn = new Button(By.xpath("//img[@src=\"/Images/design/pagesignup.png\"]"));
+    public Label alertEmailAlreadyExists = new Label(By.xpath("//span[text()='Account with this email address already exists!']"));
 
 }
